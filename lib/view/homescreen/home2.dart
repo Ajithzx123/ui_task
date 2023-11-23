9a4b925093fc6dc8938609177f3fc364dbb6888a
Ajith Kumar <ajith.kumar@realtime-innovations.com>
2023-11-23 23:54:50 +0530

    hi

--- lib/view/homescreen/home2.dart
+++ lib/view/homescreen/home2.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Home2 extends StatelessWidget {
   const Home2({super.key});
@@ -13,7 +14,8 @@ class Home2 extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Text('My Wallet',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w800),)
+              SizedBox(height: 5.h,),
+              Text('My Wallet',style: TextStyle(fontSize: 50,fontWeight: FontWeight.w800),)
             ],
           ),
         ),
